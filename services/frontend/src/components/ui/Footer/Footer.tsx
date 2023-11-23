968ffb0a3507c3b4ac2dc6ba0681841763dec06e
Xiaomei Wu <xiaomeiwu@outlook.de>
2023-11-23 15:51:41 +0100

    footer

--- services/frontend/src/components/ui/Footer/Footer.tsx
+++ services/frontend/src/components/ui/Footer/Footer.tsx
@@ -1,24 +1,11 @@
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  const { footerSection, socialMediaFlex } = styles;
+  const { footerSection } = styles;
 
   return (
     <footer className={footerSection}>
-      <div className={socialMediaFlex}>
-        <div>Sofit Logo</div>
-        <div>Social media items</div>
-        <div>Language</div>
-      </div>
-      <div>
-        <div className="3 div">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </div>
-
-        <div className="copyright">© 2023 Sofit GmbH</div>
-      </div>
+      <div className="copyright">© 2023 ❤︎ Sofit ❤︎</div>
     </footer>
   );
 }
