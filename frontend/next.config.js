1bf7717e11506221d51d0a5f2eec76eb8ab4f736
Xiaomei Wu <xiaomeiwu@outlook.de>
2023-11-25 22:41:50 +0100

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: '/api/:path*'
+        destination: `${window.location.host}/api/:path*`
       }
     ];
   },
